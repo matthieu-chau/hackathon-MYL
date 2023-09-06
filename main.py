f75b576f6a640acc5b58dde87362edf13b8f1496
nilsbaulier <134170884+nilsbaulier@users.noreply.github.com>
2023-09-06 10:29:47 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,13 +1,32 @@
-from flask import Flask, render_template, request
-
-from ask_question_to_pdf import gpt3_completion
+from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
-def template(prompt=None):
-    return render_template('index.html', prompt=prompt)
+from flask import render_template
 
-@app.route('/prompt', methods=['POST'])
+@app.route("/")
+def index(name=None):
+    return render_template('index.html', name=name)
+
+from flask import request
+
+from ask_question_to_pdf import gpt3_completion
+
+from ask_question_to_pdf import ask_question_to_pdf
+
+@app.route('/prompt',methods=['POST'])
 def prompt():
-    return {'answer':gpt3_completion(request.form['prompt'])}
+    prompt = request.form['prompt']
+    return {'answer':gpt3_completion(prompt)}
+
+@app.route('/question',methods=['GET'])
+def question():
+    return {'answer':ask_question_to_pdf()}
+
+from ask_question_to_pdf import verification
+
+@app.route('/answer',methods=['POST'])
+def answer():
+    question = request.form['question']
+    prompt = request.form['prompt']
+    return {'answer':verification(question, prompt)}
